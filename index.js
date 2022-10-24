3f444ea283ae86125de3295a524096fbe45f5b9c
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2022-10-24 10:37:02 -0600

    added questions

--- index.js
+++ index.js
@@ -8,3 +8,67 @@ const Intern = require('./lib/Intern');
 
 const teamArray = [];
 
+const managerQuestions = [
+    {
+        type: 'input',
+        name: "managerName",
+        message: "Enter the manager's name: "
+    },
+    {
+        type: 'input',
+        name: "employeeId",
+        message: "Enter the manager's employee ID number: "
+    },
+    {
+        type: 'input',
+        name: "emailAddress",
+        message: "Enter the manager's email address: "
+    },
+    {
+        type: 'input',
+        name: "officeNumber",
+        message: "Enter the manager's office number: "
+    },
+];
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: "engineerName",
+        message: "Enter the engineer's name: "
+    },
+    {
+        type: 'input',
+        name: "id",
+        message: "Enter the engineer's employee ID number: "
+    },
+    {
+        type: 'input',
+        name: "email",
+        message: "Enter the engineer's email address: "
+    },
+    {
+        type: 'input',
+        name: "gitHub",
+        message: "Enter the engineer's GitHub username: "
+    },
+];
+
+const internQuestions = [
+    {
+        name: "internName",
+        message: "Enter the intern's name: "
+    },
+    {
+        name: "id",
+        message: "Enter the intern's employee ID number: "
+    },
+    {
+        name: "email",
+        message: "Enter the intern's email address: "
+    },
+    {
+        name: "school",
+        message: "Enter the intern's school: "
+    },
+];
