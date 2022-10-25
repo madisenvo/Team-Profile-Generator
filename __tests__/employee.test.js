3517ec4fa19ebdd23a5ec988592ca957887c6ae9
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2022-10-25 13:54:00 -0600

    fixed package.json and wrote employee test

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,1 +1,20 @@
-const Employee = require('./lib/Employee');
+const Employee = require('../lib/Employee');
+
+test('Tests employee creation', () => {
+
+    const employee = new Employee('Jane Doe', 1, 'jane@jane.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test('Tests methods in employee class', () => {
+
+    const employee = new Employee('Jane Doe', 1, 'jane@jane.com');
+    
+    expect(employee.getName()).toBe(employee.name);
+    expect(employee.getId()).toBe(employee.id);
+    expect(employee.getEmail()).toBe(employee.email);
+    expect(employee.getRole()).toBe('Employee');
+});
